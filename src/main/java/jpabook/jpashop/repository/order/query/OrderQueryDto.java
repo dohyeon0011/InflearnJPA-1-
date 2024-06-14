ee2c1e1f544567e72b8033adfe77899329ea4dd2
idohyeon <ehgus5026@naver.com>
2024-06-14 22:03:59 +0900

    -Order DTO(API Response)

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -3,11 +3,13 @@ package jpabook.jpashop.repository.order.query;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(of = "orderId") // Collect에서 groupingBy 할 때 orderId를 기준으로 묶어줌.(안 하면 일대다 컬렉션 페치 조인 데이터 뻥티기 마냥 데이터 늘어남)
 public class OrderQueryDto {
 
     private Long orderId;
@@ -25,4 +27,12 @@ public class OrderQueryDto {
         this.address = address;
     }
 
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
+    }
 }
